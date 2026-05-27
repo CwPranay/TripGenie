5d53b63e298232953768bbdd925f56256acf0811
Pranay Gurav <guravpranay81@gmail.com>
2026-05-27 23:19:42 +0530

    updated frontend uri in cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://tripgeniee-ai.vercel.app"
+        "https://trip-genieai.vercel.app"
     ],
     credentials: true,
 }));
